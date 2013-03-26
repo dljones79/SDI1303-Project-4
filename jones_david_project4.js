9eaab6b284a0255a7ce31f1476077fc123343133
David Jones <dljones79@fullsail.edu>
2013-03-26 18:15:19 -0500

    Added another function.

--- jones_david_project4.js
+++ jones_david_project4.js
@@ -5,19 +5,28 @@
 //Project 4
 //Code Library
 
-// Function to check whether string is a phone number or not
+// Function to check whether string is a phone number or not.
 var phnNumChk = function(phoneNumber){
 	var isPhnNum = true;
 	var numChk = isNaN(phoneNumber[0,1,2,4,5,6,8,9,10,11]);
 	
 	if (phoneNumber.length === 12 && phoneNumber[3] === "-" && 
 		phoneNumber[7] === "-" && numChk === false) {
-			return isPhnNum;
+			return isPhnNum;									//Project 2: Boolean Output
 		} else {
 			isPhnNum = false;
 			return isPhnNum;
 		}
 }; //End of phnNumChk
 
-var chkPhnNum = phnNumChk("501-882-3810");
-console.log(chkPhnNum);
+
+//Function to check whether string is an e-mail or not.
+var emailChk = function(emailAddress){
+	var isEmail = true;
+	if (emailAddress.contains("@" && ".com")){
+		return isEmail;
+	} else {
+		var isEmail = false;
+		return isEmail;
+	}
+};
